c9ecde5678a46a526ca9d7f8fa77d1ee1c2ea941
Tai <taiu.engineer@gmail.com>
2025-04-06 13:59:31 +0900

    initial setup

--- src/Database/setup.php
+++ src/Database/setup.php
@@ -10,20 +10,7 @@ use Database\MySQLWrapper;
 
 $mysqli = new MySQLWrapper();
 
-$result = $mysqli->query("
-    CREATE TABLE IF NOT EXISTS cars (
-      id INT PRIMARY KEY AUTO_INCREMENT,
-      make VARCHAR(50),
-      model VARCHAR(50),
-      year INT,
-      color VARCHAR(20),
-      price FLOAT,
-      mileage FLOAT,
-      transmission VARCHAR(20),
-      engine VARCHAR(20),
-      status VARCHAR(10)
-    );
-");
+$result = $mysqli->query(file_get_contents(__DIR__ . '/Examples/cars-setup.sql'));
 
 if($result === false) throw new Exception('Could not execute query.');
 else print("Successfully ran all SQL setup queries.".PHP_EOL);
